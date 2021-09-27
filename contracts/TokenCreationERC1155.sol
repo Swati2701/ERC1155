b4d39cefefcc71b35dc0eb9a2db09cb552fa4ab9
Swati Jindal <88382946+Swati2701@users.noreply.github.com>
2021-09-27 14:43:54 +0530

    Add files via upload

--- contracts/TokenCreationERC1155.sol
+++ contracts/TokenCreationERC1155.sol
@@ -7,14 +7,18 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 contract TokenCreationERC1155 is ERC1155{
     uint256 public constant data1 = 1;
     uint256 public constant data2 = 2;
-
+ 
     uint256[] tokenId = [data1, data2];
+    uint tokenIds = 1;
     
+    constructor() ERC1155("https://ipfs.io/ipfs/QmXe4KrxY9ebQNToxuTKJ1JRUrtEjSSvzaFrzW1DMm6UNx/{id}.json") {}
     
-    constructor() ERC1155("https://ipfs.io/ipfs/QmQ4QVYQbPfhL7AbPh4Xx3sgRWsgg4rHkn7uaykcPVkFSA/{id}.json") {}
-    
-    function mintToken(address to, uint256[] memory initalSupply) public{
-        _mintBatch(to, tokenId, initalSupply, "");
-       
+    function mintBatchToken(address to, uint256[] memory initalSupply) public{
+        _mintBatch(to, tokenId, initalSupply, ""); 
+    }
+
+    function mintToken(address to, uint initalSupply) public{
+        _mint(to, tokenIds, initalSupply, "");
+        tokenIds++;
     }
 }
